559c17a32a2f8ce507fa211bbe5e67766f35d92c
rajat <rajat@192.168.1.6>
2017-05-08 22:57:40 +0530

    How to read input from Stdin

---
+++ HackerRank/Introduction/Reading_raw_input.py
@@ -0,0 +1,15 @@
+'''
+Created on 08-May-2017
+
+@author: Rajat Sachdeva
+'''
+
+# Reading Raw Input from STDIN
+
+def read():
+    return raw_input()
+
+def main():
+    print read()
+
+if __name__ == '__main__': main()    
