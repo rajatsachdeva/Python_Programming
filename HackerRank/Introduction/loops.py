fa1fd4ce55c063c142a797809cf6d158969910eb
Rajat Sachdeva <rajat.sachdeva621@gmail.com>
2017-05-09 13:24:28 +0530

    loops

---
+++ HackerRank/Introduction/loops.py
@@ -0,0 +1,14 @@
+'''
+Created on 09-May-2017
+
+@author: Rajat Sachdeva
+'''
+import numpy 
+
+def main():
+    n = int(raw_input())
+    for i in range(n):
+        print numpy.square(i)
+
+if __name__ == '__main__':
+    main()
