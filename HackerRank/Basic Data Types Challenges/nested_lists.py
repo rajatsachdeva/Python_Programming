1e799d54c1f67e46a4001b7a84f1392bd885de88
Rajat Sachdeva <rajat.sachdeva621@gmail.com>
2017-05-10 23:37:59 +0530

    correct

--- HackerRank/Basic Data Types Challenges/nested_lists.py
+++ HackerRank/Basic Data Types Challenges/nested_lists.py
@@ -4,23 +4,19 @@ Created on 09-May-2017
 @author: Rajat Sachdeva
 
 '''
+from __future__ import print_function
 '''
 Use of _ in loops
 It's just a variable name, and it's conventional in python to use _ for throwaway variables. 
 It just indicates that the loop variable isn't actually used.
 '''
-marksheet = []
+
 def main():
-    for _ in range(int(raw_input())):
-        _name = raw_input()
-        _score = float(raw_input())
-        temp = [_name, _score]
-        marksheet.append(temp)
-       # second_highest = sorted(list(set(marks for name)))
-    print students
-    
-second_highest = sorted(list(set([marks for name, marks in marksheet])))[1]
-print('\n'.join([a for a,b in sorted(marksheet) if b == second_highest]))
+    marksheet = []
+    for _ in range(0,int(raw_input())):
+        marksheet.append([raw_input(), float(raw_input())])
+    second_highest = sorted(list(set([marks for name, marks in marksheet])))[1]
+    print('\n'.join([a for a,b in sorted(marksheet) if b == second_highest]))
     
 if __name__ == '__main__':
     main()
